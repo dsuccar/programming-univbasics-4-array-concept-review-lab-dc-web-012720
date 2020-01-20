08a0657d894edda3de2fc9f1223cf04494dd4680
Daniel Succar <danielsuccar@icloud.com>
2020-01-20 19:14:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,11 +12,11 @@ end
 def find_max_value(array)
   x = array[0]
   array.length.times{ |index|
-if  array[index] > x
+if x < array[index]
   x == array[index]
 end
   }
-  x
+  
 end
 
 def find_min_value(array)
